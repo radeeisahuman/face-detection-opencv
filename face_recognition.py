87b59f4a3627c06bda46d6c20311324dc0da4200
Radee <radeehasadream@gmail.com>
2025-04-28 18:22:39 +0600

    Face recognizer created

---
+++ face_recognition.py
@@ -0,0 +1,32 @@
+import cv2
+import numpy as np
+import os
+
+def get_images_from_folder():
+    images_dir = os.getcwd() + '/images'
+    image_names = os.listdir(images_dir)
+    images = []
+    for i in image_names:
+        images.append(cv2.imread('images/' + i))
+
+    return images
+
+
+face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
+recognizer = cv2.face.LBPHFaceRecognizer_create()
+
+faces = []
+labels = []
+
+images = get_images_from_folder()
+
+index = 1
+
+for img in images:
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    faces.append(gray)
+    labels.append(index)
+
+recognizer.train(faces, np.array(labels))
+
+recognizer.save('trainer.yml')
--- main.py
+++ main.py
@@ -2,8 +2,10 @@ import cv2
 
 def main():
     face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
-
+    recognizer = cv2.face.LBPHFaceRecognizer_create()
     cam = cv2.VideoCapture(0)
+
+    recognizer.read('trainer.yml')
 
     #fourcc = cv2.VideoWriter_fourcc(*'mp4v')
 
@@ -15,6 +17,11 @@ def main():
         faces = face_cascade.detectMultiScale(grayscale, scaleFactor=1.1, minNeighbors=5)
 
         for (x, y, w, h) in faces:
+            face_region = grayscale[y:y + h, x:x + w]
+
+            label, confidence = recognizer.predict(face_region)
+
+            cv2.putText(frame, f'ID: {label} and Confidence: {confidence}', (x, y - 10), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 0, 0), 2)
             cv2.rectangle(frame, (x, y), (x + w, y + h), (255, 0, 0), 2)
 
         cv2.imshow('Face Detection', frame)
