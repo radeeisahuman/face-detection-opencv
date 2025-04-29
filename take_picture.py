4f70b0d12d2a9bbec575fcf4514b6307a9aa48c6
Radee <radeehasadream@gmail.com>
2025-04-29 18:28:13 +0600

    Added a flash to the taking of picture

--- take_picture.py
+++ take_picture.py
@@ -1,5 +1,6 @@
 import cv2
 import os
+import numpy as np
 
 path_to_images = os.getcwd() + '/images'
 list_of_images = os.listdir(path_to_images)
@@ -8,10 +9,13 @@ index = len(list_of_images)
 
 while True:
     ret, frame = cam.read()
+    flash = 255 * np.ones_like(frame, dtype=np.uint8)
     cv2.imshow('Take Image', frame)
     key = cv2.waitKey(1)
     if key == ord('t'):
         filename = os.path.join(path_to_images, f'image{index}.jpg')
+        cv2.imshow('Take Image', flash)
+        cv2.waitKey(100)
         cv2.imwrite(filename, frame)
         index += 1
     elif key == ord('f'):
