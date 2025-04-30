22282d04952b64b943a310644b08c8b1a9a7fc4a
Radee <radeehasadream@gmail.com>
2025-04-30 19:26:01 +0600

    Created a GUI Interface for the system

--- main.py
+++ main.py
@@ -1,13 +1,14 @@
 import cv2
 import config
-import face_recognition
+import face_recognition as fr
+import take_picture as tp
+import tkinter as tk
 
-def main():
+def take_attendance():
     face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
     recognizer = cv2.face.LBPHFaceRecognizer_create()
     cam = cv2.VideoCapture(0)
-
-    face_recognition.train()
+    
     recognizer.read('trainer.yml')
 
     #fourcc = cv2.VideoWriter_fourcc(*'mp4v')
@@ -39,6 +40,20 @@ def main():
 
     cam.release()
     cv2.destroyAllWindows()
+
+def main():
+    root = tk.Tk()
+    root.geometry("800x600")
+
+    take_picture = tk.Button(root, text="Take a new Picture", command=tp.take_picture)
+    train_model = tk.Button(root, text="Train Model with New Pictures", command=fr.train)
+    attendance_window = tk.Button(root, text="Take your attendance", command=take_attendance)
+
+    take_picture.grid(row=1, column=1, padx=10, pady=5)
+    train_model.grid(row=1, column=2, padx=10, pady=5)
+    attendance_window.grid(row=1, column=3, padx=10, pady=5)
+
+    root.mainloop()
 
 if __name__ == '__main__':
     main()
--- take_picture.py
+++ take_picture.py
@@ -2,24 +2,25 @@ import cv2
 import os
 import numpy as np
 
-path_to_images = os.getcwd() + '/images'
-list_of_images = os.listdir(path_to_images)
-cam = cv2.VideoCapture(0)
-index = len(list_of_images)
+def take_picture():
+    path_to_images = os.getcwd() + '/images'
+    list_of_images = os.listdir(path_to_images)
+    cam = cv2.VideoCapture(0)
+    index = len(list_of_images)
 
-while True:
-    ret, frame = cam.read()
-    flash = 255 * np.ones_like(frame, dtype=np.uint8)
-    cv2.imshow('Take Image', frame)
-    key = cv2.waitKey(1)
-    if key == ord('t'):
-        filename = os.path.join(path_to_images, f'image{index}.jpg')
-        cv2.imshow('Take Image', flash)
-        cv2.waitKey(100)
-        cv2.imwrite(filename, frame)
-        index += 1
-    elif key == ord('f'):
-        break
+    while True:
+        ret, frame = cam.read()
+        flash = 255 * np.ones_like(frame, dtype=np.uint8)
+        cv2.imshow('Take Image', frame)
+        key = cv2.waitKey(1)
+        if key == ord('t'):
+            filename = os.path.join(path_to_images, f'image{index}.jpg')
+            cv2.imshow('Take Image', flash)
+            cv2.waitKey(100)
+            cv2.imwrite(filename, frame)
+            index += 1
+        elif key == ord('f'):
+            break
 
-cam.release()
-cv2.destroyAllWindows()
+    cam.release()
+    cv2.destroyAllWindows()
